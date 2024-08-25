ed728121c7695be82ceedab33a6f04a877369a90
Abhishek Bohora <abhishek.bohora16@gmail.com>
2024-08-25 10:07:46 +0545

    storing token in cookie and getting from it

--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -2,6 +2,7 @@ import { create } from "zustand";
 import { z } from "zod";
 import { UserRolesEnum } from "@/constants";
 import { jwtDecode } from "jwt-decode";
+import Cookies from "js-cookie";
 
 const TokenDataSchema = z.object({
   _id: z.string(),
@@ -30,31 +31,50 @@ export const decodeAccessToken = (accessToken: string) =>
   TokenDataSchema.parse(jwtDecode<TokenData>(accessToken));
 
 export const useAuthStore = create<State & Action>((set) => ({
-  accessTkn: undefined,
-  refreshTkn: undefined,
-  isAuthenticated: false,
-  accessTokenData: undefined,
+  accessTkn: Cookies.get("accessToken") || undefined,
+  refreshTkn: Cookies.get("refreshToken") || undefined,
+  isAuthenticated: !!Cookies.get("accessToken"),
+  accessTokenData: Cookies.get("accessToken")
+    ? decodeAccessToken(Cookies.get("accessToken") as string)
+    : undefined,
+
   setAccessToken: (accessToken: string | undefined) => {
     let accessTokenData: TokenData | undefined;
     try {
-      accessTokenData = accessToken
-        ? decodeAccessToken(accessToken)
-        : undefined;
+      if (accessToken) {
+        accessTokenData = decodeAccessToken(accessToken);
+        Cookies.set("accessToken", accessToken, { expires: 1 }); // expires in 1 day
+      } else {
+        Cookies.remove("accessToken");
+      }
     } catch (error) {
       console.error(error);
       accessTokenData = undefined;
+      Cookies.remove("accessToken");
     }
     set(() => ({ accessTokenData, accessTkn: accessToken }));
   },
-  setRefreshToken: (refreshToken: string | undefined) =>
-    set(() => ({ refreshTkn: refreshToken })),
+
+  setRefreshToken: (refreshToken: string | undefined) => {
+    if (refreshToken) {
+      Cookies.set("refreshToken", refreshToken, { expires: 7 }); // expires in 7 days
+    } else {
+      Cookies.remove("refreshToken");
+    }
+    set(() => ({ refreshTkn: refreshToken }));
+  },
+
   setIsAuthenticated: (isAuth: boolean) =>
     set(() => ({ isAuthenticated: isAuth })),
-  clearTokens: () =>
+
+  clearTokens: () => {
+    Cookies.remove("accessToken");
+    Cookies.remove("refreshToken");
     set({
       accessTkn: undefined,
       accessTokenData: undefined,
       refreshTkn: undefined,
       isAuthenticated: false,
-    }),
+    });
+  },
 }));
